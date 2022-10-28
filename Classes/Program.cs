c0cb4d8c0c9e5d15ec620621dc29b2d6670840b5
heather otto <heatherotto32@yahoo.com>
2022-10-28 08:55:06 -0500

    initial commit

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "Mustang";
+            myCar.Year = 2007;
+
+            Console.WriteLine(myCar.Make);
+            Console.WriteLine(myCar.Model);
+            Console.WriteLine(myCar.Year);
         }
     }
 }
